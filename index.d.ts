dc57d3c978224f69dc977d3ba633ef71c388fbcf
Jichan <>
2019-11-21 10:02:59 +0900

    orignal source version 3.2.6

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+export { SsdpServer as Server } from './lib/server';
+export { SsdpClient as Client } from './lib/client';
+export { SSDP as Base, Headers, SSDPEvents } from './lib/index';
+import * as Const from './lib/const';
+export { Const };
